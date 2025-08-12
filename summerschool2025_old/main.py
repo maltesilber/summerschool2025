c1a394d2d0f4366d01257ad37dd185b06a89035b
maltesilber <malte.silbernagel@web.de>
2025-08-12 21:21:38 +0200

    initial commit

---
+++ summerschool2025_old/main.py
@@ -0,0 +1,71 @@
+from argparse import ArgumentParser
+import pandas as pd
+from utils import get_dataloader, collate_fn
+from transformers import ViTForImageClassification, Trainer, TrainingArguments
+
+
+def train(args):
+    meta_data = pd.read_csv('meta/metadata.csv')
+    train_data, val_data, test_data = get_dataloader(
+        meta_data,
+        args.data_root,
+        args.batch_size,
+        args.num_workers
+    )
+
+    model = ViTForImageClassification.from_pretrained(
+        "google/vit-base-patch16-224",
+        num_labels=183,
+        ignore_mismatched_sizes=True
+    )
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        metric_for_best_model="accuracy",
+        greater_is_better=True,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        learning_rate=args.lr,
+        lr_scheduler_type=args.lr_scheduler_type,
+        weight_decay=args.weight_decay,
+        num_train_epochs=args.epochs,
+        warmup_ratio=args.warmup_ratio,
+        logging_dir=args.logging_dir,
+        logging_steps=args.logging_steps,
+        report_to="tensorboard",
+        fp16=args.fp16,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        tokenizer=None,
+        data_collator=collate_fn
+    )
+    trainer.train()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--data_root', type=str, default='/home/malte/datasets/FungiImages')
+    parser.add_argument('--output_dir', type=str, default='./vit-base-checkpoints')
+    parser.add_argument('--logging_dir', type=str, default='./logs')
+
+    # hyperparams
+    parser.add_argument('--lr', type=float, default=3e-5)
+    parser.add_argument('--lr_scheduler_type', type=str, default='cosine')
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--batch_size', type=int, default=120)
+    parser.add_argument('--num_workers', type=int, default=32)
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
+    parser.add_argument('--warmup_ratio', type=float, default=0.01)
+    parser.add_argument('--logging_steps', type=int, default=50)
+    parser.add_argument('--fp16', action='store_true')
+
+    args = parser.parse_args()
+    train(args)
---
+++ summerschool2025_old/utils.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from transformers import AutoImageProcessor
+from sentence_transformers import SentenceTransformer
+
+from dataset import FungiDataset
+
+
+def embed_string(x: pd.Series) -> pd.Series:
+    unique_tokens = x.unique()
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = model.encode(unique_tokens)
+    embedding_map = dict(zip(unique_tokens, embeddings))
+    return x.map(embedding_map)
+
+def encode_datetime(x: pd.Series) -> tuple:
+    x = pd.to_datetime(x)
+    x_year = x.dt.year
+    day_of_year = x.dt.dayofyear
+    angle = 2 * np.pi * (day_of_year - 1) / 365.25
+    return x_year, np.sin(angle), np.cos(angle)
+
+
+def collate_fn(batch):
+    images, labels, file_paths = zip(*batch)
+    images = torch.stack(images)
+    labels = torch.tensor(labels, dtype=torch.long)
+    return {"pixel_values": images, "labels": labels}
+
+
+def get_transforms(split):
+    processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+    if split == "train":
+        return transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(p=0.2),
+            transforms.RandomRotation(degrees=15),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=processor.image_mean, std=processor.image_std)
+        ])
+    else:
+        return transforms.Compose([
+        transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=processor.image_mean, std=processor.image_std)
+        ])
+
+def get_dataloader(df, path, batch_size=32, num_workers=32):
+    train_df = df[df['filename_index'].str.contains('train')]
+    train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
+    test_df = df[df['filename_index'].str.contains('test')]
+
+    train_dataset = FungiDataset(df=train_df, path=path, transform=get_transforms('train'))
+    val_dataset = FungiDataset(df=val_df, path=path, transform=get_transforms('val'))
+    test_dataset = FungiDataset(df=test_df, path=path, transform=get_transforms('test'))
+    return train_dataset, val_dataset, test_dataset
